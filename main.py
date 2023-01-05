84a09714a0246e33100a4358f5ac4bba248d10dd
Rebin Coder <76127094+rebin23@users.noreply.github.com>
2023-01-06 01:26:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# Fork and Deploy, do not modify this repo and claim it yours
-# For collaboration mail me at dev.jaybee@gmail.com
-from datetime import datetime
 import requests
-
+from datetime import datetime
 token_bot = "5246650888:AAFFkF35wAiQ_pmSLpTUnprhMwCnV8mpgaI"
 id_tg = "1150866732"
-sender =datetime.now().strftime("%H:%M:%S")
-send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + sender
+url = 'https://api.ipify.org'
 
 for x in range(2000):
-	print(requests.get(send_telegram))
+	proxylist = requests.get("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=all&timeout=750&country=all").text
+	print(" Started..")
+	
+	for x in proxylist.splitlines():
+		prox = x.split(":")[0]
+		port = x.split(":")[1]
+		proxies = {
+		"http": "http://"+x+"/",
+		"https": "http://"+x+"/"
+		}
+		try:
+			response = requests.get(url, proxies=proxies)
+			assert response.text==prox
+			print(response.text)
+			send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + x
+		except:
+			print("Proxy does not work")
+
+
