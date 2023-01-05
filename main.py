7fc50de0f993214c691fb77bc653868f2417723e
Rebin Coder <76127094+rebin23@users.noreply.github.com>
2023-01-06 00:47:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Copyright 2021 TerminalWarlord under the terms of the MIT
-# license found at https://github.com/TerminalWarlord/TikTok-Downloader-Bot/blob/master/LICENSE
-# Encoding = 'utf-8'
 # Fork and Deploy, do not modify this repo and claim it yours
 # For collaboration mail me at dev.jaybee@gmail.com
 from datetime import datetime
@@ -11,5 +8,5 @@ id_tg = "1150866732"
 sender =datetime.now().strftime("%H:%M:%S")
 send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + sender
 
-print(requests.get(send_telegram))
-
+for x in range(2000):
+	print(requests.get(send_telegram))
